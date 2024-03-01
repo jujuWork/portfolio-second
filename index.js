95680ab175445d48eaff375a396a595bb689c032
bb8ZgjUjU <joharrywork@gmail.com>
2024-03-01 23:06:10 +0900

    03012024

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
-// ALERT
-
-alert ("Please use the latest Chrome to view the page") void;
-alert ("Not yet Compatible with other browser") void;
-
-
-// SKILL ANIMATION 
-
 const observer = new IntersectionObserver(entries => {
     entries.forEach(entry => {
         if(entry.isIntersecting){
-            document.querySelectorAll(".skill-container")[0].classList.add("fadeInLeft");
-            document.querySelectorAll(".skill-container")[1].classList.add("fadeInTop");
-            document.querySelectorAll(".skill-container")[2].classList.add("fadeInRight");
+            document.querySelectorAll(".animated")[0].classList.add("fadeInLeft");
+            document.querySelectorAll(".animated")[1].classList.add("fadeInTop");
+            document.querySelectorAll(".animated")[2].classList.add("fadeInRight");
         }
     })
 })
 
-//observer selector
 
-observer.observe(document.querySelector("#Skills"));       
+const  
+
+// ALERT
+
+alert ("Please use the latest Chrome to view the page");
+alert ("Not yet Compatible with other browser");
+
+
+// SKILL ANIMATION 
+
+     
