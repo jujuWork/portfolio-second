42dab079b429e34d832720de29da36f12679738d
bb8ZgjUjU <joharrywork@gmail.com>
2024-03-04 22:31:59 +0900

    03042024

--- index.js
+++ index.js
@@ -1,22 +1,2 @@
-const observer = new IntersectionObserver(entries => {
-    entries.forEach(entry => {
-        if(entry.isIntersecting){
-            document.querySelectorAll(".animated")[0].classList.add("fadeInLeft");
-            document.querySelectorAll(".animated")[1].classList.add("fadeInTop");
-            document.querySelectorAll(".animated")[2].classList.add("fadeInRight");
-        }
-    })
-})
-
-
-const  
-
-// ALERT
-
-alert ("Please use the latest Chrome to view the page");
-alert ("Not yet Compatible with other browser");
-
-
-// SKILL ANIMATION 
-
-     
+alert('Please use the latest google chrome browser')
+alert('Browser compatability issue')
