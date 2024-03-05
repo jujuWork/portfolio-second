5e9c776ce815229da26f515b85f5e1181a193473
bb8ZgjUjU <joharrywork@gmail.com>
2024-03-05 23:06:50 +0900

    03052024

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 alert('Please use the latest google chrome browser')
 alert('Browser compatability issue')
+
+
+// SCROLL ANIMATION
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach ((entry) => { // checking of multiple entry
+        console.log(entry) //condition check
+        if (entry.isIntersecting) { //  checking if its intersecting the viewport or not
+                entry.target.classList.add('show'); //if tis intersecting will add a class show, if only once animated you can stop here!!
+        } else {
+                entry.target.classList.remove('show') // removing the animation if is intersecting
+            }
+    });
+});
+
+
+const hiddenElements = document.querySelectorAll('.skillContainer'); // selecting all the classlist to be animated
+hiddenElements.forEach((el) => observer.observe(el));
