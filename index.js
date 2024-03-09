84e190eb63be5a211205615157384a92b4f0f47b
bb8ZgjUjU <joharrywork@gmail.com>
2024-03-09 22:57:39 +0900

    03092024

--- index.js
+++ index.js
@@ -5,16 +5,16 @@
 // SCROLL ANIMATION
 
 const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => { // checking of multiple entry
-        console.log(entry) //condition check
-        if (entry.isIntersecting) { //  checking if its intersecting the viewport or not
-            entry.target.classList.add('show'); //if tis intersecting will add a class show, if only once animated you can stop here!!
+    entries.forEach((entry) => {    // MULTIPLE ENTRIES
+        console.log(entry)
+        if (entry.isIntersecting) { // CONDITIONAL CHECKING
+            entry.target.classList.add('show'); // STOP HERE IF ONE LOOP ONLY
         } else {
-            entry.target.classList.remove('show'); // removing the animation if is intersecting
+            entry.target.classList.remove('show'); // REMOVING THE CLASS SHOW WHEN IS NOT INTERSECTING
         }
     });
 });
 
 
-const skillContainerElements = document.querySelectorAll('.skillContainer'); // selecting all the classlist to be animated
-skillContainer.forEach((el) => observer.observe(el));
+const animateElements = document.querySelectorAll('.animate'); // SELECTING ALL HIDDEN CLASS ELEMENTS
+animateElements.forEach((el) => observer.observe(el)); // OBSERVING ALL HIDDEN ELEMENTS
